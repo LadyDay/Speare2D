66345445c9a2c6aa45f4767820d0395e43899394
LadyDay <day_13k@yahoo.com>
2015-10-19 11:50:33 -0300

    commit inicial com tela de inicio - theater

---
+++ Speare2D/GameScene.swift
@@ -0,0 +1,41 @@
+//
+//  GameScene.swift
+//  Speare2D
+//
+//  Created by Dayane Kelly Rodrigues da Silva on 19/10/15.
+//  Copyright (c) 2015 LadyDay. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        //let light_1 = self.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
