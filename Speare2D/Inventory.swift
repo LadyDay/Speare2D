702058fc433abe34e81de8eb9475bd1df3d71552
LadyDay <day_13k@yahoo.com>
2015-10-21 10:05:18 -0300

    criando classe de inventário.

---
+++ Speare2D/Inventory.swift
@@ -0,0 +1,9 @@
+//
+//  Inventory.swift
+//  Speare2D
+//
+//  Created by Dayane Kelly Rodrigues da Silva on 21/10/15.
+//  Copyright © 2015 LadyDay. All rights reserved.
+//
+
+import Foundation
--- Speare2D/StartScene.swift
+++ Speare2D/StartScene.swift
@@ -86,46 +86,6 @@ class StartScene: SKScene {
                 }
             }
         }
-/*
-        let touchObj = touches.first as UITouch!
-        locationTouch = touchObj.locationInNode(self)
-        //let firefox = childNodeWithName("FirefoxNode") as! SKSpriteNode
-        let nodeTouched = nodeAtPoint(locationTouch)
-        
-        //Rapousa vai andar até onde tocar
-        let moveMainCharacter = SKAction .moveToX(locationTouch.x, duration: 1.0)
-        
-        if nodeTouched.name == "Tutorial" {
-            print("Espelho 1")
-            //Rapousa anda até lá
-            mainCharacter.runAction(moveMainCharacter, completion: {
-                //Muda cena para Opção1
-                let fadeScene = SKTransition.fadeWithDuration(1.5)
-                self.gameScene = TutorialScene(fileNamed: "TutorialScene")
-                self.view?.presentScene(self.gameScene!, transition: fadeScene)
-            })
-        } else if nodeTouched.name == "Option2"{
-            print("Objeto 2")
-            //Rapousa anda até lá
-            mainCharacter.runAction(moveMainCharacter, completion: {
-                //Muda cena para Opção2
-                //let fadeScene = SKTransition.fadeWithDuration(1.5)
-                //self.gameScene = OptionTwo(fileNamed: "OptionTwo")
-                //self.view?.presentScene(self.gameScene!, transition: fadeScene)
-            })
-        } else if nodeTouched.name == "Option3"{
-            print("Objeto 3")
-            //Rapousa anda até lá
-            mainCharacter.runAction(moveMainCharacter, completion: {
-                //Muda cena para Opção3
-                //let fadeScene = SKTransition.fadeWithDuration(1.5)
-                //self.gameScene = OptionThree(fileNamed: "OptionThree")
-                //self.view?.presentScene(self.gameScene!, transition: fadeScene)
-            })
-        } else {
-            mainCharacter.runAction(moveMainCharacter)
-        }
-*/
     }
 
     /* Called before each frame is rendered */
