7a4b4e9dbb4e23f68d66b20cd3014b76ce88d910
LadyDay <day_13k@yahoo.com>
2015-10-23 11:02:35 -0300

    Criando a classe do inventario e add o swipe para aparecer/desaparecer o inventario.

--- Speare2D/Inventory.swift
+++ Speare2D/Inventory.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2015 LadyDay. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class Inventory: SKScene {
+    
+    var gameScene: SKScene!
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let swipeUp: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "swipeUp")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view!.addGestureRecognizer(swipeUp)
+    }
+    
+    func swipeUp(sender: UISwipeGestureRecognizer){
+        let nodeTest = self.nodeAtPoint(sender.locationInView(self.view))
+        if(nodeTest.name=="viewCloset"){
+            self.gameScene = TutorialScene(fileNamed: "TutorialScene")
+            self.view?.presentScene(self.gameScene!)
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            let node = self.nodeAtPoint(location) as! SKSpriteNode
+            
+            self.clearLots()
+            
+            node.color = UIColor.blackColor()
+            
+        }
+    }
+    
+    func clearLots(){
+        for(var i = 0; i<10; i++){
+            let lot = self.childNodeWithName("lot\(i)") as! SKSpriteNode
+            lot.color = UIColor.redColor()
+        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+    }
+}
--- Speare2D/TutorialScene.swift
+++ Speare2D/TutorialScene.swift
@@ -6,9 +6,40 @@
 //  Copyright © 2015 LadyDay. All rights reserved.
 //
 
-import UIKit
 import SpriteKit
 
 class TutorialScene: SKScene {
+    
+    var gameScene: SKScene!
+    
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let swipeDown: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "swipeDown")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view?.addGestureRecognizer(swipeDown)
+        /*
+        let swipeInventoryUp: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "swipeInventoryUp")
+        swipeInventoryUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view!.addGestureRecognizer(swipeInventoryUp)
+        */
+    }
+    
+    func swipeDown(sender: UISwipeGestureRecognizer){
+        let nodeTest = self.nodeAtPoint(sender.locationInView(self.view))
+        if(nodeTest.name=="viewCloset"){
+            self.gameScene = Inventory(fileNamed: "Inventory")
+            self.view?.presentScene(self.gameScene!)
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
 
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+    }
 }
