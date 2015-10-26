577b105b9c2c247a19e102358c0b62f029663bb7
LadyDay <day_13k@yahoo.com>
2015-10-26 08:25:58 -0300

    apresentando o inventário em cenas (tutorial)

--- Speare2D/TutorialScene.swift
+++ Speare2D/TutorialScene.swift
@@ -10,21 +10,25 @@ import SpriteKit
 
 class TutorialScene: SKScene {
     
+    var viewInventory: SKView!
     var gameScene: SKScene!
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        self.viewInventory = SKView(frame: CGRectMake(0, 0, 1024, 150))
         let swipeDown: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "swipeDown:")
         swipeDown.direction = UISwipeGestureRecognizerDirection.Down
         self.view?.addGestureRecognizer(swipeDown)
     }
     
     func swipeDown(sender: UISwipeGestureRecognizer){
+        self.view?.addSubview(viewInventory as UIView)
         //for nodeTest in self.nodesAtPoint(sender.locationInView(self.view)){
           //  print("\(nodeTest.name)")
             //if(nodeTest.name=="viewCloset"){
+                let transition = SKTransition.moveInWithDirection(SKTransitionDirection.Down, duration: 2)
                 self.gameScene = Inventory(fileNamed: "Inventory")
-                self.view?.presentScene(self.gameScene)
+                viewInventory.presentScene(gameScene, transition: transition)
             //}
         //}
     }
