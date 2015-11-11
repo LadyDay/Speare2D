6946387402c917197ee6020d29995a711d2c70aa
alnp <alessandrapereira@outlook.com>
2015-11-11 10:05:44 -0300

    Alterando a optionScene.

--- Speare2D/SceneDefault.swift
+++ Speare2D/SceneDefault.swift
@@ -19,10 +19,22 @@ class SceneDefault: SKScene {
     var mainCharacter: Alex = Alex()
     
     //music
-    var backgroundMusic: SKAudioNode!
-    var applauseEffect: SKAudioNode!
+    var backgroundMusic: SKAudioNode = SKAudioNode(fileNamed: "backgroundMusic.mp3")
+    var bgMusicVolume: Float!
+    var effectsMusic: SKAudioNode = SKAudioNode(fileNamed: "effectsound.aiff")
+    var effectsVolume: Float!
     let pauseAction = SKAction.pause()
     let stopAction = SKAction.stop()
+    
+    //    struct Volume{
+    //        var bgMusicVolume: Float = 0.7
+    //        var effectsVolume: Float = 0.7
+    //
+    //        init(bgMusicVolume: Float, effectsVolume: Float) {
+    //            self.bgMusicVolume = bgMusicVolume
+    //            self.effectsVolume = effectsVolume
+    //        }
+    //    }
     
     /* ANOTHER FUNCTION */
     func moveInfo(gameScene: SceneDefault){
@@ -32,5 +44,15 @@ class SceneDefault: SKScene {
         self.mainCharacter.removeFromParent()
     }
     
+    func musicBgConfiguration(fileString: String) {
+        //        let gameScene = OptionsScene(fileNamed: "OptionScene")
+        //        gameScene!.bgMusicVolume = self.bgMusicVolume
+        backgroundMusic = SKAudioNode(fileNamed: fileString)
+        backgroundMusic.autoplayLooped = true
+        self.addChild(backgroundMusic)
+        backgroundMusic.runAction(SKAction.changeVolumeTo(bgMusicVolume, duration: 0))
+        
+        
+    }
 }
 
