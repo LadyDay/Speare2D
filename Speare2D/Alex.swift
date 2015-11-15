c7e215ebf3376609662d5e471a039602579a69cb
alnp <alessandrapereira@outlook.com>
2015-11-15 20:06:23 -0300

    Arrumei a classe de Alex um pouco

--- Speare2D/Alex.swift
+++ Speare2D/Alex.swift
@@ -13,13 +13,22 @@ class Alex: SKSpriteNode {
     let alexTextureAtlas = SKTextureAtlas(named: "AlexCorrendo.atlas")
     var alexSpriteArray = Array<SKTexture>()
     var waitingAlexSpriteArray = Array<SKTexture>()
-    var waitingAlexSpriteArray2 = Array<SKTexture>()
     var scaleAlex: CGFloat = 0.3
     
     init() {
+        //Initializing alex with her first sprite
         let texture = SKTexture(imageNamed: "Alex_Sprite_Princ1_400x708")
         super.init(texture: texture, color: SKColor.clearColor(), size: texture.size())
-
+        
+        //Appending sprites for animation
+        initWalkingSprites()
+        initBlinkingSprites()
+        
+        //Making alex blink
+        blinkingAlex()
+    }
+    
+    func initWalkingSprites(){
         alexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Corr1_400x708"))
         alexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Corr2_400x708"))
         alexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Corr3_400x708"))
@@ -30,16 +39,9 @@ class Alex: SKSpriteNode {
         alexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Corr8_400x708"))
         alexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Corr9_400x708"))
         alexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Corr10_400x708"))
-        
-        animate()
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    private func animate(){
-        
+    func initBlinkingSprites(){
         waitingAlexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Princ1_400x708"))
         waitingAlexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Princ1_400x708"))
         waitingAlexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Princ1_400x708"))
@@ -48,12 +50,26 @@ class Alex: SKSpriteNode {
         waitingAlexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Princ1_400x708"))
         waitingAlexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Princ2_400x708"))
         waitingAlexSpriteArray.append(alexTextureAtlas.textureNamed("Alex_Sprite_Princ1_400x708"))
-        
-        let playerAnimation = SKAction.repeatActionForever( SKAction.animateWithTextures(waitingAlexSpriteArray, timePerFrame: 0.2))
-        self.runAction(playerAnimation)
+    }
+
+    //It is necessary because of the init() function
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    //Function to the default position of Alex
+    func setupAlex(){
+        self.position = CGPoint(x:167, y:200)
+        self.zPosition = 10.0
+        self.xScale = scaleAlex
+        self.yScale = scaleAlex
     }
     
     
+    
+    ////MOTION FUNCTIONS
+    
+    //Functions to implement
     func jump(){
         
     }
@@ -62,17 +78,16 @@ class Alex: SKSpriteNode {
         
     }
     
-    func setupAlex(){
-        self.position = CGPoint(x:167, y:200)
-        self.zPosition = 10.0
-        self.xScale = scaleAlex
-        self.yScale = scaleAlex
+    //Blink eyes - Waiting animation
+    func blinkingAlex(){
+        let playerAnimation = SKAction.repeatActionForever( SKAction.animateWithTextures(waitingAlexSpriteArray, timePerFrame: 0.2))
+        self.runAction(playerAnimation)
     }
     
+    //Function to walk
     func walk(inicialLocation: CGPoint, touchLocation: CGPoint, tamSize: CGFloat, objectPresent: Bool, objectSize: CGSize?) -> SKAction {
         
         self.removeActionForKey("andando")
-        
         let pastLocation = inicialLocation
         var currentLocation: CGPoint
         
@@ -94,7 +109,6 @@ class Alex: SKSpriteNode {
         
         let duration : NSTimeInterval = makeDuration(currentLocation, pastLocation: pastLocation)/400
         let moveToPoint = SKAction.moveToX(currentLocation.x, duration: duration)
-        
         let walkingAlexAction = SKAction.repeatActionForever(SKAction.animateWithTextures(self.alexSpriteArray, timePerFrame: 0.08, resize: true, restore: true))
         let direction = SKAction.scaleXTo((directionCharacter(currentLocation, pastLocation: pastLocation)), duration: 0)
         let sequence = SKAction.sequence([SKAction.group([direction, moveToPoint]), SKAction.runBlock({self.removeActionForKey("andando")})])
@@ -103,15 +117,18 @@ class Alex: SKSpriteNode {
         return group
     }
     
+    //Function to determine which way Alex should point.
     func directionCharacter(currentLocation : CGPoint, pastLocation: CGPoint) -> CGFloat{
-        //let xVar: Int
         if (currentLocation.x - pastLocation.x) > 0 {
+            
             return scaleAlex
         } else {
+            
             return -scaleAlex
         }
     }
     
+    //Function for timing compass
     func makeDuration(currentLocation : CGPoint, pastLocation: CGPoint) -> NSTimeInterval{
         let catetos:CGFloat = pow(abs(currentLocation.x - pastLocation.x), 2) + pow(abs(currentLocation.y - pastLocation.y), 2)
         let hipotenusa = sqrt(catetos)
